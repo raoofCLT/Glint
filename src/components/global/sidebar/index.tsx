002803b1c7299a77f3e78f5f2cf4d2af741e0cc7
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-27 13:15:11 +0530

    feat(sidebar)Implement some features to sidebar

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -12,13 +12,22 @@ import {
 } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 import { useQueryData } from "@/hooks/userQueryData";
-import { WorkspaceProps } from "@/types/index.type";
+import { NotificationProps, WorkspaceProps } from "@/types/index.type";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import Modal from "../modal";
-import { PlusCircle } from "lucide-react";
+import { Menu, PlusCircle } from "lucide-react";
 import Search from "../search";
+import { MENU_ITEMS } from "@/constants";
+import SidebarItem from "./sidebar-items";
+import { getNotifications } from "@/actions/user";
+import WorkspacePlaceholder from "./workspace-placeholder";
+import GlobalCard from "../global-card";
+import { Button } from "@/components/ui/button";
+import Loader from "../loader";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import InfoBar from "../info-bar";
 
 type Props = {
   activeWorkspaceId: string;
@@ -26,9 +35,17 @@ type Props = {
 
 const Sidebar = ({ activeWorkspaceId }: Props) => {
   const router = useRouter();
+  const pathName = usePathname();
   const { data, isFetched } = useQueryData(["user-workspaces"], getWorkSpaces);
+  const menuitems = MENU_ITEMS(activeWorkspaceId);
+
+  const { data: notifications } = useQueryData(
+    ["user-notifications"],
+    getNotifications
+  );
 
   const { data: workspace } = data as WorkspaceProps;
+  const { data: count } = notifications as NotificationProps;
 
   const onChangeActiveWorkspace = (value: string) => {
     router.push(`/dashboard/${value}`);
@@ -38,7 +55,7 @@ const Sidebar = ({ activeWorkspaceId }: Props) => {
     (s) => s.id === activeWorkspaceId
   );
 
-  return (
+  const SidebarSection = (
     <div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
       <div className="bg-[#111111] p-4 flex gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0">
         <Image src="/glint-logo.svg" height={40} width={40} alt="logo" />
@@ -76,31 +93,110 @@ const Sidebar = ({ activeWorkspaceId }: Props) => {
           </SelectValue>
         </SelectTrigger>
       </Select>
-      {currentWorkspace?.type === "PUBLIC" &&  workspace.subscription?.plan == "PRO" &&(
-        <Modal
-          trigger={
-            <span className="text-sm cursor-pointer flex items-center justify-center bg-neutral-800/90 hover:bg-neutral-800/60 w-full rounded-sm p-[5px] gap-2">
-              <PlusCircle
-                size={15}
-                className="text-neutral-800/90 fill-neutral-500"
-              />
-              <span className="text-neutral-400 font-semibold text-xs">
-                Invite To Workspace
+      {currentWorkspace?.type === "PUBLIC" &&
+        workspace.subscription?.plan == "PRO" && (
+          <Modal
+            trigger={
+              <span className="text-sm cursor-pointer flex items-center justify-center bg-neutral-800/90 hover:bg-neutral-800/60 w-full rounded-sm p-[5px] gap-2">
+                <PlusCircle
+                  size={15}
+                  className="text-neutral-800/90 fill-neutral-500"
+                />
+                <span className="text-neutral-400 font-semibold text-xs">
+                  Invite To Workspace
+                </span>
               </span>
-            </span>
-          }
-          title="Invite To Workspace"
-          description="Invite other user to your workspace"
-        >
-          <Search workspaceId={activeWorkspaceId} />
-        </Modal>
-      )}
+            }
+            title="Invite To Workspace"
+            description="Invite other user to your workspace"
+          >
+            <Search workspaceId={activeWorkspaceId} />
+          </Modal>
+        )}
       <p className="w-full text-[#9D9D9D] font-bold mt-4">Menu</p>
-    <nav className="w-full">
-      <ul></ul>
-    </nav>
+
+      <nav className="w-full">
+        <ul>
+          {menuitems.map((item) => (
+            <SidebarItem
+              key={item.title}
+              href={item.href}
+              icon={item.icon}
+              selected={pathName === item.href}
+              title={item.title}
+              notification={
+                (item.title === "Notification" &&
+                  count._count &&
+                  count._count.notification) ||
+                0
+              }
+            />
+          ))}
+        </ul>
+      </nav>
+      <Separator className="w-4/5" />
+      <p className="w-full text-[#9D9D9D] font-bold mt-4">Workspace</p>
+
+      {workspace.workspace.length === 1 && workspace.members.length === 0 && (
+        <div className="w-full mt-[-10px]">
+          <p className="text-[#3c3c3c] font-medium text-sm">
+            {workspace.subscription?.plan === "FREE"
+              ? "Upgrade to create workspaces"
+              : "No workspaces"}
+          </p>
+        </div>
+      )}
+
+      <nav className="w-full">
+        <ul className="h-[150px] overflow-auto overflow-x-hidden fade-layer">
+          {workspace.workspace.length > 0 &&
+            workspace.workspace.map((item) => (
+              <SidebarItem
+                key={item.name}
+                title={item.name}
+                href={`/dashboard/${item.id}`}
+                selected={pathName === `/dashboard/${item.id}`}
+                notification={0}
+                icon={
+                  <WorkspacePlaceholder>
+                    {item.name.charAt(0)}
+                  </WorkspacePlaceholder>
+                }
+              />
+            ))}
+        </ul>
+      </nav>
+      <Separator className="w-4/5" />
+      {workspace.subscription?.plan === 'FREE' && (
+        <GlobalCard
+          title="Upgrade to Pro"
+          description="Unlock AI features like transcription, AI summary, and more."
+          footer={
+            <Button className="text-sm w-full mt-2">
+              <Loader color="#000" state={false}>Upgrade</Loader>
+            </Button>
+          }
+        />
+      )}
+    </div>
+  );
+  return (
+    <div className="full">
+      <InfoBar />
+      <div className="md:hidden fixed my-4">
+        <Sheet>
+          <SheetTrigger asChild className="ml-2">
+            <Button variant={"ghost"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={"left"} className="p-0 w-fit h-full">
+            {SidebarSection}
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="md:block hidden h-full">{SidebarSection}</div>
     </div>
   );
 };
-
 export default Sidebar;
